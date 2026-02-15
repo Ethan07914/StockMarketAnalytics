227c8cfae9b1ab0f9eb98d3df2a1d71be8ccd8df
EthanPook10 <Ethan.Pook10@outlook.com>
2026-02-15 17:30:59 +0000

    My first commit

---
+++ client.py
@@ -0,0 +1,55 @@
+from massive import RESTClient
+import config
+from urllib3 import HTTPResponse
+from typing import cast
+import pandas as pd
+
+class client:
+    def __init__(self, config_file_name):
+        self.config_file_name = config_file_name
+
+    def ConfigureClient(self):
+        return RESTClient(self.config_file_name.api_key)
+
+class stock:
+    def __init__(self, client, ticker, multiplier, interval, date_from, date_to, raw=True):
+        self.client = client
+        self.ticker = ticker
+        self.multiplier = multiplier
+        self.interval = interval
+        self.date_from = date_from
+        self.date_to = date_to
+        self.raw = raw
+
+    def get_data(self):
+        aggs = cast(
+            HTTPResponse,
+            self.client.get_aggs(
+                self.ticker,
+                self.multiplier,
+                self.interval,
+                self.date_from,
+                self.date_to,
+                self.raw
+            ))
+        return aggs
+
+    def format_data(self):
+        df = pd.DataFrame(self.get_data())
+        df['ticker'] = self.ticker
+        df['date'] = pd.to_datetime(df['timestamp'], unit='ms').dt.date
+        return df[['ticker', 'date', 'open', 'high', 'low', 'close', 'volume', 'vwap', 'timestamp', 'transactions', 'otc']]
+        #VWAP (Volume Weighted Average Price)
+
+client = client(config).ConfigureClient()
+tickers = ['AAPL', 'GOOG', 'MSFT', 'AMZN', 'NVDA']
+for ticker in tickers:
+    stock_data = stock(client, ticker, '1', 'day', '2026-01-13', '2026-02-13')
+    if ticker == tickers[0]:
+        data = stock_data.format_data()
+    else:
+        data = pd.concat([data, stock_data.format_data()])
+print(data)
+stock_data.format_data().to_csv(f'StockPrices_{stock_data.date_from}-{stock_data.date_to}.csv', index=False)
+
+
---
+++ main.py
@@ -0,0 +1,63 @@
+import requests
+import config
+import pandas as pd
+
+base_url = "https://api.polygon.io/v2/reference/news"
+
+class news:
+    def __init__(self, url, ticker, interval, date_from, date_to, api_key, order):
+        self.url = url
+        self.ticker = ticker
+        self.interval = interval
+        self.date_from = date_from
+        self.date_to = date_to
+        self.api_key = api_key
+        self.order = order
+
+    def set_parameters(self):
+        parameters = {
+            "ticker": self.ticker,
+            "interval": self.interval,
+            "date_from": self.date_from,
+            "order": self.order,
+            "date_to": self.date_to,
+            "apiKey": self.api_key
+        }
+        return parameters
+
+    def get_data(self):
+        return requests.get(self.url, self.set_parameters()).json()
+
+    def unnest_insight(self, row):
+        subframe = pd.DataFrame(row['insights'])
+        row['sentiment'] = subframe['sentiment'].loc[subframe['ticker'] == self.ticker].iloc[0]
+        #.iloc[0] prevents the sub data frames index from being included
+        row['sentiment_reasoning'] = subframe['sentiment_reasoning'].loc[subframe['ticker'] == self.ticker].iloc[0]
+        return row
+
+    def format_data(self):
+        data = self.get_data()
+        articles = []
+        for article in data['results']:
+            articles.append(article)
+        df = pd.DataFrame(articles)
+        df['publisher'] = df.apply(lambda row: row['publisher']['name'], axis=1)
+        df['date'] = df['published_utc'].str[:10]
+        df['ticker'] = self.ticker
+        df = df.apply(self.unnest_insight, axis=1)
+        return df[['ticker', 'publisher', 'title', 'author', 'date', 'description', 'sentiment', 'sentiment_reasoning']]
+
+tickers = ['AAPL', 'GOOG', 'MSFT', 'AMZN', 'NVDA']
+for ticker in tickers:
+    news_data = news(base_url, ticker, 'day', '2026-01-01', '2026-02-01', config.api_key, 'desc')
+    if ticker == tickers[0]:
+        data = news_data.format_data()
+    else:
+        data = pd.concat([data, news_data.format_data()])
+data = data.reset_index(drop=True)
+print(data[['ticker', 'sentiment']])
+# data.to_parquet('StockNews.gzip', index=False)
+# parquet = pd.read_parquet('StockNews.gzip')
+# print(parquet.head())
+
+
